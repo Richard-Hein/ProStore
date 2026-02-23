db4444c9b4bc8cbbcbe21f5d02f3e0ac9a3dd298
Richard Hein <pyaesone.dev.web@gmail.com>
2026-02-23 20:45:01 +0630

    fixed any error

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,12 @@
 import ProductList from "@/components/shared/product/product-list";
-// import sampleData from "@/db/sample-data";
+import sampleData from "@/db/sample-data";
 import {getLatestProducts} from "@/lib/actions/product.action"
 export const metadata = {
   title: 'Home'
 };
 const Home = async () => {
   const latestProductsRaw = await getLatestProducts();
-  const latestProducts = latestProductsRaw.map((product: any) => ({
+  const latestProducts = latestProductsRaw.map((product) => ({
     ...product,
     price: product.price.toString(),
     // If rating is also a Decimal and Product expects string, convert it as well:
