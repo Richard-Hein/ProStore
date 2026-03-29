40034480501ac2061f06b815bd84bad58c0306a2
Richard Hein <pyaesone.dev.web@gmail.com>
2026-03-29 13:42:41 +0630

    fix shipping address error

--- app/(root)/shipping-address/shipping-address-form.tsx
+++ app/(root)/shipping-address/shipping-address-form.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { Controller, useForm, SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { shippingAddressSchema } from "@/lib/validators";
@@ -6,7 +7,7 @@ import { ShippingAddress } from "@/types";
 import { useRouter } from "next/navigation";
 import { z } from "zod";
 import { shippingAddressDefaultValues } from "@/lib/constants";
-import { useTransition } from "react";
+import { useState } from "react";
 import {
   Field,
   FieldError,
@@ -19,165 +20,168 @@ import { ArrowRight, Loader } from "lucide-react";
 import { updateUserAddress } from "@/lib/actions/user.actions";
 import { toast } from "sonner";
 
-const ShippingAddressForm = ({ address }: { address: ShippingAddress }) => {
+const ShippingAddressForm = ({
+  address,
+}: {
+  address?: ShippingAddress;
+}) => {
   const form = useForm<z.infer<typeof shippingAddressSchema>>({
     resolver: zodResolver(shippingAddressSchema),
-    defaultValues: address || shippingAddressDefaultValues,
-    // defaultValues: address,
+    defaultValues: address ?? shippingAddressDefaultValues,
   });
 
   const router = useRouter();
-  const [isPending, startTransition] = useTransition();
+  const [isPending, setIsPending] = useState(false);
 
-  const onSubmit:SubmitHandler<z.infer<typeof shippingAddressSchema>> = async (values) => {
-    startTransition(async()=> {
-        const res = await updateUserAddress(values);
-        if(!res.success){
-            toast.error(res.message)
-        }
-        router.push("/payment-method");
-    })
+  const onSubmit: SubmitHandler<z.infer<typeof shippingAddressSchema>> = async (
+    values
+  ) => {
+    try {
+      setIsPending(true);
+
+      const res = await updateUserAddress(values);
+
+      if (!res.success) {
+        toast.error(res.message);
+        return;
+      }
+
+      router.push("/payment-method");
+    } catch (error) {
+      toast.error("Something went wrong while saving your address.");
+      console.error(error);
+    } finally {
+      setIsPending(false);
+    }
   };
 
   return (
-    <>
-      <div className="max-w-md mx-auto space-y-4">
-        <h1 className="h2-bold mt-4">Shipping Address</h1>
-        <p className="text-sm text-muted-foreground">
-          Please enter address to ship to
-        </p>
-        <form
-          {...form}
-          //   id="address-form"
-          method="post"
-          className="space-y-4"
-          onSubmit={form.handleSubmit(onSubmit)}
-        >
-          <div className="flex flex-col gap-5 md:flex-row">
-            <FieldGroup>
-              {/* full name */}
-              <Controller
-                name="fullName"
-                control={form.control}
-                render={({ field, fieldState }) => (
-                  <Field data-invalid={fieldState.invalid}>
-                    <FieldLabel htmlFor="form-rhf-demo-fullName">
-                      Full Name
-                    </FieldLabel>
-                    <Input
-                      {...field}
-                      id="form-rhf-demo-fullName"
-                      aria-invalid={fieldState.invalid}
-                      placeholder="Enter Full Name"
-                      autoComplete="off"
-                    />
-                    {fieldState.invalid && (
-                      <FieldError errors={[fieldState.error]} />
-                    )}
-                  </Field>
+    <div className="max-w-md mx-auto space-y-4">
+      <h1 className="h2-bold mt-4">Shipping Address</h1>
+      <p className="text-sm text-muted-foreground">
+        Please enter address to ship to
+      </p>
+
+      <form
+        className="space-y-4"
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <div className="flex flex-col gap-5 md:flex-row">
+          <FieldGroup>
+            <Controller
+              name="fullName"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="fullName">Full Name</FieldLabel>
+                  <Input
+                    {...field}
+                    id="fullName"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Enter Full Name"
+                    autoComplete="name"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+
+            <Controller
+              name="streetAddress"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="streetAddress">Street Address</FieldLabel>
+                  <Input
+                    {...field}
+                    id="streetAddress"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Enter Street Address"
+                    autoComplete="street-address"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+
+            <Controller
+              name="city"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="city">City</FieldLabel>
+                  <Input
+                    {...field}
+                    id="city"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Enter City"
+                    autoComplete="address-level2"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+
+            <Controller
+              name="postalCode"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="postalCode">Postal Code</FieldLabel>
+                  <Input
+                    {...field}
+                    id="postalCode"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Enter Postal Code"
+                    autoComplete="postal-code"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+
+            <Controller
+              name="country"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="country">Country</FieldLabel>
+                  <Input
+                    {...field}
+                    id="country"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="Enter Country"
+                    autoComplete="country-name"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+
+            <div className="flex gap-2">
+              <Button type="submit" disabled={isPending}>
+                {isPending ? (
+                  <Loader className="w-4 h-4 animate-spin" />
+                ) : (
+                  <ArrowRight className="h-4 w-4" />
                 )}
-              />
-              {/* street address */}
-              <Controller
-                name="streetAddress"
-                control={form.control}
-                render={({ field, fieldState }) => (
-                  <Field data-invalid={fieldState.invalid}>
-                    <FieldLabel htmlFor="form-rhf-demo-streetAddress">
-                      Street Address
-                    </FieldLabel>
-                    <Input
-                      {...field}
-                      id="form-rhf-demo-streetAddress"
-                      aria-invalid={fieldState.invalid}
-                      placeholder="Enter Street Address"
-                      autoComplete="off"
-                    />
-                    {fieldState.invalid && (
-                      <FieldError errors={[fieldState.error]} />
-                    )}
-                  </Field>
-                )}
-              />
-              {/* city */}
-              <Controller
-                name="city"
-                control={form.control}
-                render={({ field, fieldState }) => (
-                  <Field data-invalid={fieldState.invalid}>
-                    <FieldLabel htmlFor="form-rhf-demo-city">City</FieldLabel>
-                    <Input
-                      {...field}
-                      id="form-rhf-demo-city"
-                      aria-invalid={fieldState.invalid}
-                      placeholder="Enter City"
-                      autoComplete="off"
-                    />
-                    {fieldState.invalid && (
-                      <FieldError errors={[fieldState.error]} />
-                    )}
-                  </Field>
-                )}
-              />
-              {/* postal code */}
-              <Controller
-                name="postalCode"
-                control={form.control}
-                render={({ field, fieldState }) => (
-                  <Field data-invalid={fieldState.invalid}>
-                    <FieldLabel htmlFor="form-rhf-demo-postalCode">
-                      Postal Code
-                    </FieldLabel>
-                    <Input
-                      {...field}
-                      id="form-rhf-demo-postalCode"
-                      aria-invalid={fieldState.invalid}
-                      placeholder="Enter Postal Code"
-                      autoComplete="off"
-                    />
-                    {fieldState.invalid && (
-                      <FieldError errors={[fieldState.error]} />
-                    )}
-                  </Field>
-                )}
-              />
-              {/* country */}
-              <Controller
-                name="country"
-                control={form.control}
-                render={({ field, fieldState }) => (
-                  <Field data-invalid={fieldState.invalid}>
-                    <FieldLabel htmlFor="form-rhf-demo-country">
-                      Country
-                    </FieldLabel>
-                    <Input
-                      {...field}
-                      id="form-rhf-demo-country"
-                      aria-invalid={fieldState.invalid}
-                      placeholder="Enter Country"
-                      autoComplete="off"
-                    />
-                    {fieldState.invalid && (
-                      <FieldError errors={[fieldState.error]} />
-                    )}
-                  </Field>
-                )}
-              />
-              <div className="flex gap-2">
-                <Button type="submit" disabled={isPending}>
-                  {isPending ? (
-                    <Loader className="w-4 h-4 animate-spin" />
-                  ) : (
-                    <ArrowRight className="h-4 w-4" />
-                  )}{" "}
-                  Continue
-                </Button>
-              </div>
-            </FieldGroup>
-          </div>
-        </form>
-      </div>
-    </>
+                Continue
+              </Button>
+            </div>
+          </FieldGroup>
+        </div>
+      </form>
+    </div>
   );
 };
 
