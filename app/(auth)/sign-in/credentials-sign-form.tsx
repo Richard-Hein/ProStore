a46591dde9e65819d6d9ec48b2e00ebd935fce86
Richard-Hein <pyaesone.dev.web@gmail.com>
2026-06-20 18:22:14 +0630

    feat: show admin  login credential for temporary

--- app/(auth)/sign-in/credentials-sign-form.tsx
+++ app/(auth)/sign-in/credentials-sign-form.tsx
@@ -9,69 +9,77 @@ import { useActionState } from "react";
 import { useFormStatus } from "react-dom";
 import { useSearchParams } from "next/navigation";
 
-
 const CredentialsSignInForm = () => {
-    const [data, action] = useActionState(signInWithCredentials, {
-        success: false,
-        message: ''
-    });
+  const [data, action] = useActionState(signInWithCredentials, {
+    success: false,
+    message: "",
+  });
 
-    const searchParams = useSearchParams();
-    const callbackUrl = searchParams.get('callbackUrl') || '/' ;
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl") || "/";
 
+  const SignInButton = () => {
+    const { pending } = useFormStatus();
+    return (
+      <Button className="w-full" variant={"default"}>
+        {pending ? "Signing In..." : "Sign In"}{" "}
+      </Button>
+    );
+  };
+  return (
+    <form action={action}>
+      <input type="hidden" name="callbackUrl" value={callbackUrl} />
+      <div className="space-y-6">
+        <div className="bg-green-300/50 h-[100px] rounded-md shadow-lg space-y-3 flex flex-col items-center justify-center">
+          <div className="text-black dark:text-white  text-lg font-bold">
+            Admin Account Credentials
+          </div>
+          <div className="text-black dark:text-white font-semibold text-sm">
+            Email - admin@gmail.com
+          </div>
+          <div className="text-black dark:text-white font-semibold text-sm">
+            Password - 123456
+          </div>
+        </div>
+        <div className="space-y-2">
+          <Label htmlFor="email">Email</Label>
+          <Input
+            id="email"
+            name="email"
+            type="email"
+            placeholder="Enter your email"
+            required
+            autoComplete="email"
+            defaultValue={signInDefaultValues.email}
+          />
+        </div>
+        <div className="space-y-2">
+          <Label htmlFor="password">Password</Label>
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            placeholder="Enter your password"
+            required
+            autoComplete="password"
+            defaultValue={signInDefaultValues.password}
+          />
+        </div>
+        <div>
+          <SignInButton />
+        </div>
+        {data && !data.success && (
+          <div className="text-center text-destructive">{data.message}</div>
+        )}
+        <div className="text-sm text-center text-muted-foreground">
+          Don&apos;t have an account?{" "}
+          <Link href="/sign-up" target="_self" className="link">
+            Sign Up
+          </Link>
+        </div>
+      </div>
+    </form>
+  );
+};
 
-    const SignInButton = () => {
-        const {pending} = useFormStatus();
-        return (
-            <Button className="w-full" variant={"default"}> 
-            {
-                pending ? "Signing In..." : "Sign In"
-            } </Button>
-        )
-    }
-    return ( 
-        <form action={action}>
-            <input type="hidden" name="callbackUrl"  value={callbackUrl}/>
-            <div className="space-y-6">
-                <div className="space-y-2">
-                    <Label htmlFor="email">Email</Label>
-                    <Input 
-                    id="email"
-                    name="email"
-                    type="email"
-                    required
-                    autoComplete="email"
-                    defaultValue={signInDefaultValues.email}
-                    />
-
-                </div>
-                <div className="space-y-2">
-                    <Label htmlFor="password">Password</Label>
-                    <Input 
-                    id="password"
-                    name="password"
-                    type="password"
-                    required
-                    autoComplete="password"
-                    defaultValue={signInDefaultValues.password}
-                    />
-
-                </div>
-                <div>
-                    <SignInButton/>
-                </div>
-                {data && !data.success && (
-                    <div className="text-center text-destructive">
-                        {data.message}
-                    </div>
-                )}
-                <div className="text-sm text-center text-muted-foreground">
-                    Don&apos;t have an account? {' '}
-                    <Link href="/sign-up" target="_self" className="link">Sign Up</Link>
-                </div>
-            </div>
-        </form>
-     );
-}
- 
 export default CredentialsSignInForm;
